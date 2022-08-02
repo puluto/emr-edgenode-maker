10be2dcac207cf357adf9d330d2f6ee520e53d19
Puluto <puluto@gmail.com>
2022-08-02 18:00:20 +0800

    Update make-emr-edge-node.sh

--- make-emr-edge-node.sh
+++ make-emr-edge-node.sh
@@ -10,31 +10,35 @@ showUsage() {
   printHeading "[ MAKE EMR EDGE NODE SCRIPTS ] USAGE"
 
   echo "# 说明：初始化（只需执行一次）, 安装基础软件包，emr基础app, 配置emr repo, 创建hadoop用户, 创建必要文件夹"
-  echo "$0 init [PEM_FILE_PATH] [MASTER_NODE_IP]"
+  echo "$0 init ${PEM_FILE_PATH} ${MASTER_NODE_IP}"
   echo
 
   echo "# 说明：制作hadoop客户端"
-  echo "$0 make-hadoop-client [PEM_FILE_PATH] [MASTER_NODE_IP]"
+  echo "$0 make-hadoop-client ${PEM_FILE_PATH} ${MASTER_NODE_IP}"
   echo
 
   echo "# 说明：制作spark客户端"
-  echo "$0 make-spark-client [PEM_FILE_PATH] [MASTER_NODE_IP]"
+  echo "$0 make-spark-client ${PEM_FILE_PATH} ${MASTER_NODE_IP}"
   echo
 
   echo "# 说明：制作hive客户端"
-  echo "$0 make-hive-client [PEM_FILE_PATH] [MASTER_NODE_IP]"
+  echo "$0 make-hive-client ${PEM_FILE_PATH} ${MASTER_NODE_IP}"
   echo
 
   echo "# 说明：制作hbase客户端"
-  echo "$0 make-hbase-client [PEM_FILE_PATH] [MASTER_NODE_IP]"
+  echo "$0 make-hbase-client ${PEM_FILE_PATH} ${MASTER_NODE_IP}"
+  echo
+
+  echo "# 说明：制作flink客户端"
+  echo "$0 make-flink-client ${PEM_FILE_PATH} ${MASTER_NODE_IP}"
   echo
 
   echo "# 说明：制作oozie客户端"
-  echo "$0 make-oozie-client [PEM_FILE_PATH] [MASTER_NODE_IP]"
+  echo "$0 make-oozie-client ${PEM_FILE_PATH} ${MASTER_NODE_IP}"
   echo
 
   echo "# 说明：制作hudi客户端"
-  echo "$0 make-hudi-client"
+  echo "$0 make-hudi-client ${PEM_FILE_PATH} ${MASTER_NODE_IP}"
   echo
 
   echo "# 说明：制作sqoop客户端"
@@ -55,11 +59,13 @@ init() {
   masterNode="$2"
   chmod 600 "$pemFile"
   makeYumRepo "$pemFile" "$masterNode"
-  yum -y install vim wget zip unzip expect tree htop iotop nc telnet lrzsz openssl-devel emrfs emr-ddb emr-goodies emr-kinesis emr-s3-select emr-scripts emr-puppet mysql-connector-java
+  yum -y install vim wget zip unzip expect tree htop iotop nc telnet lrzsz openssl-devel emrfs emr-ddb emr-goodies emr-kinesis emr-s3-select emr-scripts emr-puppet
   makeHadoopUser "$pemFile"
   makeDir
   makeJdk
   echo "Plase REBOOT!!"
+  # custom
+  yum install pig phoenix* -y
 }
 
 makeUser() {
@@ -112,7 +118,7 @@ makeDir() {
 makeJdk() {
   yum -y install java-1.8.0-openjdk
   tee /etc/profile.d/java.sh <<EOF
-export JAVA_HOME=/etc/alternatives/jre
+export JAVA_HOME=/etc/alternatives/java_sdk
 export PATH=$JAVA_HOME/bin:$PATH
 EOF
 }
@@ -122,6 +128,10 @@ makeHadoopClient() {
   masterNode="$2"
   yum -y install hadoop-client hadoop-lzo
   rsync -avz --delete -e "ssh -o StrictHostKeyChecking=no -o ServerAliveInterval=10 -i $pemFile" hadoop@$masterNode:'/etc/hadoop/conf/*' /etc/hadoop/conf
+  # custom
+  yarn rmadmin -addToClusterNodeLabels "CORE(exclusive=false)"
+  hdfs dfs -chmod -R 777 /tmp/
+  hdfs dfs -chmod 777 /user
 }
 
 makeHiveClient() {
@@ -141,10 +151,12 @@ makeSparkClient() {
   yum -y install spark-core spark-python spark-datanucleus
   rsync -avz --delete -e "ssh -o StrictHostKeyChecking=no -o ServerAliveInterval=10 -i $pemFile" hadoop@$masterNode:'/etc/spark/conf/*' /etc/spark/conf
   echo "spark.hadoop.yarn.timeline-service.enabled false" | tee -a /etc/spark/conf/spark-defaults.conf
+  echo 'export SPARK_DIST_CLASSPATH=$(hadoop classpath)' | tee -a /etc/spark/conf/spark-env.conf
   mkdir -p /var/log/spark/user
   chmod 777 -R /var/log/spark/user
 }
 
+# custom
 makeFlinkClient() {
   pemFile="$1"
   masterNode="$2"
@@ -152,6 +164,10 @@ makeFlinkClient() {
   rsync -avz --delete -e "ssh -o StrictHostKeyChecking=no -o ServerAliveInterval=10 -i $pemFile" hadoop@$masterNode:'/etc/flink/conf/*' /etc/flink/conf
   echo "fs.overwrite-files: true" | tee -a /etc/flink/conf/flink-conf.yaml
   echo "s3.path.style.access: true" | tee -a /etc/flink/conf/flink-conf.yaml
+  echo "classloader.check-leaked-classloader: false" | tee -a /etc/flink/conf/flink-conf.yaml
+  sed -i '#^jobmanager.web.upload.dir:#s#:.*#: /mnt/tmp/flink-upload#' /etc/flink/conf/flink-conf.yaml
+  sed -i '#^yarn.properties-file.location:#s#:.*#: /mnt/tmp/flink-yarn#' /etc/flink/conf/flink-conf.yaml
+  sed -i "/^historyserver.web.address:/s/:.*/: $EMR_MASTER/" /etc/flink/conf/flink-conf.yaml
 }
 
 makeHBaseClient() {
@@ -164,6 +180,8 @@ makeHBaseClient() {
 }
 
 makeHudiClient() {
+  pemFile="$1"
+  masterNode="$2"
   yum -y install hudi
   rsync -avz --delete -e "ssh -o StrictHostKeyChecking=no -o ServerAliveInterval=10 -i $pemFile" hadoop@$masterNode:'/etc/hudi/conf/*' /etc/hudi/conf
 }
@@ -172,6 +190,11 @@ makeSqoopClient() {
   yum -y install sqoop
   ln -sf /usr/lib/hive/lib/libthrift-0.9.3.jar /usr/lib/sqoop/lib/
   ln -sf /etc/hive/conf/hive-site.xml /etc/sqoop/conf/
+  # custom
+  cd /tmp/
+  wget https://downloads.mysql.com/archives/get/p/3/file/mysql-connector-java-5.1.49.tar.gz -O mysql-connector-java-5.1.49.tar.gz
+  tar -xf mysql-connector-java-5.1.49.tar.gz
+  sudo mv mysql-connector-java-5.1.49/mysql-connector-java-5.1.49-bin.jar /usr/lib/sqoop/lib/
 }
 
 makeOozieClient() {
@@ -217,7 +240,8 @@ case $1 in
     makeFlinkClient "$@"
     ;;
   make-hudi-client)
-    makeHudiClient
+    shift
+    makeHudiClient "$@"
     ;;
   make-sqoop-client)
     makeSqoopClient
